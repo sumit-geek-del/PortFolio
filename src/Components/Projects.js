7017bc745926551e6dd12e87d8a9a4aa1dd97cbb
unknown <sumitrajraj508@gmail.com>
2022-02-02 15:48:58 +0530

    Final

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './style.css'
-import downloaded from './images/download (1).png'
-import notes from './images/25-255518_notes-icon-notes-app-icon-png-transparent-png-removebg-preview.png'
-import textUtils from './images/2ce9d0f65984573db389e4f6c85dd8a5-removebg-preview.png'
+import React from "react";
+import "./style.css";
+import corona from './images/og-corona-removebg-preview.png'
+import movie from './images/5fdfa98001a201aed81d5119d0456915-removebg-preview.png'
+import quotes from './images/id.jl.android.quoteseverything-removebg-preview (1).png'
 
 export default function Projects() {
   return (
@@ -12,38 +12,80 @@ export default function Projects() {
         <p className="project-p">Some of my own Projects</p>
 
         <div className="cards">
-
-
-          <div className="card text-center" >
-            <img src={downloaded} className="card-img-top" alt="..." />
+          <div className="card text-center">
+            <img src={corona} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">BMI Calculator</h5>
-              <p className="card-text">BMI calculator helps the user to calculate their BMI index.BMI is a measurement of a person's leanness or corpulence based on their height and weight, and is intended to quantify tissue mass. </p>
-              <a href="https://sumit-geek-del.github.io/bmi-calculator/" target="_blank" rel="noreferrer" className="btn btn-outline-dark projects-btn">Visit Website</a>
-              <a href="https://github.com/sumit-geek-del/bmi-calculator" target="_blank" rel="noreferrer" className="btn btn-outline-dark projects-btn">Source Code</a>
-            </div>
-          </div>
-          <div className="card text-center" >
-            <img src={notes} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">MyNotes</h5>
-              <p className="card-text">MyNotes is a simple web application that allows user to take notes while working. It is very much easy to use and store all the data in the local storage.</p>
-              <a href="https://sumit-geek-del.github.io/notes.github.io/" target="_blank" rel="noreferrer" className="btn btn-outline-dark projects-btn">Visit Website</a>
-              <a href="https://github.com/sumit-geek-del/notes.github.io" target="_blank" rel="noreferrer" className="btn btn-outline-dark projects-btn">Source Code</a>
+              <h5 className="card-title">CoronaTracker</h5>
+              <p className="card-text">
+                CoronaTracker allows users to access real-time information about
+                corona cases all over the world. It's really easy to use and has
+                a straightforward design. It's created with Angular CLI and
+                pulls data from the disease.sh API. We used charts to keep
+                things simple for the users. {" "}
+              </p>
+              <a
+                href="https://sumit-geek-del.github.io/coronaTracker/"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark projects-btn"
+              >
+                Visit Website
+              </a>
+              <a
+                href="https://github.com/sumit-geek-del/coronaTracker"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark projects-btn"
+              >
+                Source Code
+              </a>
             </div>
           </div>
           <div className="card text-center">
-            <img src={textUtils} className="card-img-top" alt="..." />
+            <img src={movie} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">TextUtils</h5>
-              <p className="card-text">TextUtils is a user-friendly website which help users to easily gets insights about thier text. Users can easily transform their text using this web application.</p>
+              <h5 className="card-title">MovieLove</h5>
+              <p className="card-text">
+              We designed a web app for movie and series fans that allows them to simply search for their favourite films and read user reviews to assist them pick what to watch and what not to watch. It's made with Reactjs, OMDB API, and Styled Components.
+              </p>
+              <a
+                href="https://github.com/sumit-geek-del/ReactMovieApp"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark projects-btn"
+              >
+                Source Code
+              </a>
+            </div>
+          </div>
+          <div className="card text-center">
+            <img src={quotes} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">QuotesGenerator</h5>
+              <p className="card-text">
+              A simple web tool to keep you inspired throughout the day. Allow you to generate quotes and then tweet them to Twitter.
+              </p>
+              <a
+                href="https://sumit-geek-del.github.io/QuotesGenerator/"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark projects-btn"
+              >
+                Visit Webiste
+              </a>
 
-              <a href="https://github.com/sumit-geek-del/TextUtils-React" target="_blank" rel="noreferrer" className="btn btn-outline-dark projects-btn">Source Code</a>
+              <a
+                href="https://github.com/sumit-geek-del/QuotesGenerator"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark projects-btn"
+              >
+                Source Code
+              </a>
             </div>
           </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
